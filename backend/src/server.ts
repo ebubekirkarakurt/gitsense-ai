84cac3157e79bbfee1f94c7b96d636d6719df0b6
ebubekirkarakurt <ebukar75@gmail.com>
2026-06-20 21:58:13 +0300

    fix: dotenv() moved to top

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import commitRouter from "./routes/commit";
 
-dotenv.config();
 
 const app = express();
 
